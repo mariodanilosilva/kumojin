b0735eab9293330253534dbdf18b740811589bd3
Mário Silva <guri.vds@bol.com.br>
2021-08-07 16:51:38 -0400

    make interface controller

--- backend/src/presentation/controllers/timezone.ts
+++ backend/src/presentation/controllers/timezone.ts
@@ -1,7 +1,8 @@
+import { Controller } from '../protocols/controller'
 import { HttpResponse } from '../protocols/http'
 import { ok } from '../helpers/http-helper'
 
-export class TimezoneController {
+export class TimezoneController implements Controller {
   handle (): HttpResponse {
     return ok('any_data')
   }
